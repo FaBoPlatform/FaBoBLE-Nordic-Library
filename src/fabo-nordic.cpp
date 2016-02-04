fb5a1e8886562f96ca3e3b73fc682a35c8e2af4b
tsuchiya <takashi@gclue.jp>
2016-02-04 15:29:16 +0900

    added sd_ble_enable

--- src/fabo-nordic.cpp
+++ src/fabo-nordic.cpp
@@ -1,5 +1,53 @@
 #include "fabo-nordic.h"
 
-void NordicBLE::init()
-{
+// initalize
+#ifdef USE_HARDWARE_SERIAL
+	void NordicBLE::init(HardwareSerial &serial, bool debug)
+	{
+		this->serial = &serial;
+		this->debug = debug;
+		serial.begin(NRF_BAUD_RATE);
+	}
+#else
+	void NordicBLE::init(SoftwareSerial &serial, bool debug)
+	{
+		this->serial = &serial;
+		this->debug = debug;
+		serial.begin(NRF_BAUD_RATE);
+	}
+#endif
+
+// sd_ble_enable
+void NordicBLE::sd_ble_enable(uint8_t service_changed, uint32_t attr_tab_size) {
+	byte data[] = {
+		0x01, service_changed, 0x00, 0x00, 0x00, 0x00
+	};
+	memcpy(&data[2], &attr_tab_size, 4);
+	this->sendCommand(0x60, data, 6);
+}
+
+// Send command
+void NordicBLE::sendCommand(byte type, byte *data, int size) {
+	// Packet size
+	int len = size + 2;
+	serial->write(len);
+	serial->write(len << 8);
+	// type: 0=command
+	serial->write((byte)0);
+	// Command type
+	serial->write(type);
+	// Data
+	serial->write(data, size);
+	// Debug
+	if (debug) {
+		Debug.println("*sendCommand");
+		Debug.print("type:");
+		Debug.println(type, HEX);
+		Debug.print("data:");
+	  for (int i=0; i<size; i++) {
+			Debug.print(data[i], HEX);
+			if (i<size-1) Debug.print(",");
+	  }
+		Debug.println("");
+	}
 }
--- src/fabo-nordic.h
+++ src/fabo-nordic.h
@@ -1,7 +1,28 @@
 #include "Arduino.h"
 
+// #define USE_HARDWARE_SERIAL
+#define NRF_BAUD_RATE 38400
+#define Debug Serial
+
+#ifndef USE_HARDWARE_SERIAL
+  #include <SoftwareSerial.h>
+#endif
+
 class NordicBLE
 {
+private:
+  #ifdef USE_HARDWARE_SERIAL
+    HardwareSerial *serial;
+  #else
+    SoftwareSerial *serial;
+  #endif
+  bool debug = false;
+  void sendCommand(byte type, byte *data, int size);
 public:
-  void init(void);
+  #ifdef USE_HARDWARE_SERIAL
+    void init(HardwareSerial &serial, bool debug = false);
+  #else
+    void init(SoftwareSerial &serial, bool debug = false);
+  #endif
+  void sd_ble_enable(uint8_t service_changed = 0, uint32_t attr_tab_size = 0);
 };
